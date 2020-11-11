60fb4ac5f4cf283dc04bc4936d0b145b444ed65b
Jose Ruiz <jotadram6@gmail.com>
2020-11-10 21:30:30 -0500

    1D histo generator

--- RootHistoGenerator.py
+++ RootHistoGenerator.py
@@ -56,3 +56,41 @@ def HistoProducerGaussOverExpo(HistoN,xbins,xmin,xmax,ybins,ymin,ymax,NB=10000,N
         Backgr0DHistos.append(b0)
         SigpBk0DHistos.append(n0)
     return Signal2DHistos, Backgr2DHistos, SigpBk2DHistos, Signal1DHistos, Backgr1DHistos, SigpBk1DHistos, Signal0DHistos, Backgr0DHistos, SigpBk0DHistos
+
+
+def SimpleHistoProducerGaussOverExpo(HistoN,xbins,xmin,xmax,NB=10000,NS=1000,
+                               expsets=[100.0,0.0,-0.0005],
+                               gausssets=[1.0,650,10.0]):
+    Signal1DHistos=[]
+    Backgr1DHistos=[]
+    SigpBk1DHistos=[]
+    Signal0DHistos=[]
+    Backgr0DHistos=[]
+    SigpBk0DHistos=[]
+    for i in range(HistoN):
+        BackgroundModel = ROOT.TH1F( 'bkg', 'My background model', xbins, xmin, xmax)
+        F1D = ROOT.TF1("f2","([0]*expo(x,[1..2]))",xmin, xmax)
+        F1D.SetParameters(expsets[0],expsets[1],expsets[2])
+        BackgroundModel.FillRandom("f2",NB)
+        SignalModelforN = ROOT.TH1F( 'sigforN', 'My signal model', xbins, xmin, xmax)
+        signalfforN = ROOT.TF1("signalf", "gausn(x, [0..2])",xmin, xmax)
+        signalfforN.SetParameters(gausssets[0],gausssets[1],gausssets[2])
+        SignalModelforN.FillRandom("signalf",NS)
+        BkgpSigModel = BackgroundModel.Clone("bkgpsig")
+        BkgpSigModel.Sumw2()
+        BkgpSigModel.Add(SignalModelforN,1.0)
+        n1=[]; b1=[]; s1=[]
+        for i in range(1,BackgroundModel.GetNbinsX()):
+            n1.append(float(BkgpSigModel.GetBinContent(BkgpSigModel.GetBin(i))))
+            s1.append(float(SignalModelforN.GetBinContent(SignalModelforN.GetBin(i))))
+            b1.append(float(BackgroundModel.GetBinContent(BackgroundModel.GetBin(i))))
+        n0=[sum(n1)]
+        s0=[sum(s1)]
+        b0=[sum(b1)]
+        Signal1DHistos.append(s1)
+        Backgr1DHistos.append(b1)
+        SigpBk1DHistos.append(n1)
+        Signal0DHistos.append(s0)
+        Backgr0DHistos.append(b0)
+        SigpBk0DHistos.append(n0)
+    return Signal1DHistos, Backgr1DHistos, SigpBk1DHistos, Signal0DHistos, Backgr0DHistos, SigpBk0DHistos
